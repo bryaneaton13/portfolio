4aaacb42caaf202adb3b50441e956353b425702c
Bryan Eaton <bryaneaton13@gmail.com>
2022-12-21 16:08:35 -0500

    Setup blog

---
+++ apps/blog/data/headerNavLinks.ts
@@ -0,0 +1,7 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Posts' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'About' },
+];
+
+export default headerNavLinks;
---
+++ apps/blog/pages/about.tsx
@@ -0,0 +1,17 @@
+// import { MDXLayoutRenderer } from '@/components/MDXComponents'
+import { InferGetStaticPropsType } from 'next';
+import { allAuthors } from 'contentlayer/generated';
+import { MDXLayoutRenderer } from 'pliny/mdx-components';
+import { MDXComponents } from '@/components/MDXComponents';
+
+const DEFAULT_LAYOUT = 'AuthorLayout';
+
+export const getStaticProps = async () => {
+  const author = allAuthors.find((p) => p.slug === 'default');
+  return { props: { author } };
+};
+
+export default function About({ author }: InferGetStaticPropsType<typeof getStaticProps>) {
+  if (!author) return null;
+  return <MDXLayoutRenderer layout={author?.layout || DEFAULT_LAYOUT} content={author} MDXComponents={MDXComponents} />;
+}
