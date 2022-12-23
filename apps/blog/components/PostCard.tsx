d796b0509950ae8a24e3a8abc1cbb2d86677bd05
Bryan Eaton <bryaneaton13@gmail.com>
2022-12-23 13:04:43 -0500

    Remove blog hover bg color

--- apps/blog/components/PostCard.tsx
+++ apps/blog/components/PostCard.tsx
@@ -3,7 +3,7 @@ import { Post } from '../helpers/types';
 
 function PostCard({ title, date, description, slug }: Post): JSX.Element {
   return (
-    <div className="p-4 border-b hover:bg-gray-100 dark:hover:bg-gray-800 hover:scale-[1.005] transition-transform">
+    <div className="p-4 border-b hover:scale-[1.005] transition-transform">
       <Link href="/[slug]" className="" as={`/${slug}`}>
         <h3 className="font-bold text-2xl">{title}</h3>
         <div className="italic text-sm mt-1">{date}</div>
