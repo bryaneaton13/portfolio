adfcab3bcb2928f2869bed109e2c39f6fef200e0
Bryan Eaton <bryaneaton13@gmail.com>
2023-02-14 22:23:12 -0500

    More shared utils

--- packages/shared-utils/src/index.ts
+++ packages/shared-utils/src/index.ts
@@ -14,4 +14,12 @@ export const Links = {
   Blog: Configs.isLocal ? 'http://localhost:3000' : 'https://blog.bryaneaton.com',
 };
 
-export const uniqArr = (arr: any[]) => [...new Set(arr)];
+export const uniqArr = <T>(arr: T[]): T[] => [...new Set(arr)];
+export const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
+export const exists = (v: any) => typeof v !== 'undefined';
+export const isArray = (a: any) => Array.isArray(a);
+export const isString = (s: any) => typeof s === 'string';
+export const isInteger = (n: any) => Number.isInteger(n);
+export const isObject = (o: any) => typeof o === 'object' && o !== null;
+export const isObjectEmpty = (o: { [s: string]: unknown } | ArrayLike<unknown>) =>
+  Object.entries(o).length === 0 && o.constructor === Object;
